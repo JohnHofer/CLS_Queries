9e5c74bad0932127760d0c36f5caa7538be546d1
Tyler <bowty94@gmail.com>
2015-04-18 23:03:11 -0600

    Added generic tag_type search function and wrapper

--- DB_scripts/Helpers/Helpers.php
+++ DB_scripts/Helpers/Helpers.php
@@ -207,4 +207,37 @@ function is_possible_enum_val($val,$field)
 	}
 	return false;
 }
+
+function search_by_generic_tag_type($str,$tag_type)
+{
+	global $mysqli;
+	
+	//Throws out useless words and parses string
+	$str = strtolower($str);
+	include 'Stoplist.php';
+	$str = preg_replace($stoplist, "", $str);
+	$words = $str.explode(" ",$str);
+	
+	//Build results array
+	$results = array();
+	foreach($words as $word)
+	{
+		$query_result = $mysqli->query("SELECT mediaitem_id FROM itemtag
+										JOIN tag ON tag_id = tag.id
+										JOIN mediaitem ON mediaitem_id = mediaitem.id
+										WHERE name = $word AND tag.type = '$tag_type'");
+		if($error = check_sql_error($query_result))
+			return $error;
+		while($row = $query_result->fetch_assoc())
+		{
+			$id = $row['mediaitem_id'];
+			if(array_key_exists($id,$results))
+				$results[$id]++;
+			else
+				$results[$id] = 1;
+		}
+		arsort($results);
+		return array_keys($results);
+	}
+}
 ?>
